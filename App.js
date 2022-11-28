faf29e927d44160dc332c81b8e5d882e59082a96
muhammedfatihsener <119266945+muhammedfatihsener@users.noreply.github.com>
2022-11-28 03:27:13 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <nav><h1>Murat ilk react denemesine hoşgeldin</h1><ul>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul></nav>
+  );
+}
+
+export default App;
